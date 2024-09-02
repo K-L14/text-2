88fb1112a0d5f2a5e844fd155380e666bdebc49e
K-L14 <chh_lno1@163.com>
2024-09-02 16:13:19 +0800

    Add files via upload

---
+++ text 2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder 编辑器
+
+这是一个临时脚本文件。
+"""
+
+word1 = '"hello"'
+word2 = '"world"'
+sentence = word1.strip('"') + ' ' + word2.strip('"') + '!'
+
+print( 'The first word is %s, and the second word is %s' %(word1, word2))
+print (sentence)
